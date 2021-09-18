cd022039209e7ade51d8e0bfb515fafb8a94c058
muhammad-muchson <muchson@alterra.id>
2021-09-18 15:09:25 +0700

    app ja
    memberikan comment pada app js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { PersistGate } from "redux-persist/integration/react"
 
 import Home from "./component/Home"
 import { store, persistor } from "./store/store"
-
+//app js
 function App() {
   return (
     <div className="App">
